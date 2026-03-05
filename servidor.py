773fc5ba99afb6469f26c658f3d616a22542d716
Danilo062603 <164100549+Danilo062603@users.noreply.github.com>
2026-03-05 11:32:03 -0500

    Actualización servidor

--- servidor.py
+++ servidor.py
@@ -1,45 +1,41 @@
-from http.server import HTTPServer, BaseHTTPRequestHandler
+from http.server import BaseHTTPRequestHandler, HTTPServer
 import json
 import os
 
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 
-mascotas = [
-    {"id": 1, "nombre": "Luna", "especie": "Perro", "edad": 3},
-    {"id": 2, "nombre": "Milo", "especie": "Gato", "edad": 2},
-    {"id": 3, "nombre": "Rocky", "especie": "Perro", "edad": 5}
-]
-
-class Servidor(BaseHTTPRequestHandler):
+class MiServidor(BaseHTTPRequestHandler):
 
     def do_GET(self):
 
         if self.path == "/":
+            ruta = os.path.join(BASE_DIR, "index.html")
 
-            with open(os.path.join(BASE_DIR, "index.html"), "rb") as f:
+            with open(ruta, "rb") as f:
                 contenido = f.read()
 
             self.send_response(200)
-            self.send_header("Content-Type", "text/html")
+            self.send_header("Content-type", "text/html")
             self.end_headers()
             self.wfile.write(contenido)
 
         elif self.path == "/api/mascotas":
 
+            mascotas = [
+                {"nombre": "Luna", "tipo": "Perro", "edad": 3},
+                {"nombre": "Milo", "tipo": "Gato", "edad": 2},
+                {"nombre": "Rocky", "tipo": "Perro", "edad": 5}
+            ]
+
             datos = json.dumps(mascotas).encode()
 
             self.send_response(200)
-            self.send_header("Content-Type", "application/json")
+            self.send_header("Content-type", "application/json")
             self.end_headers()
             self.wfile.write(datos)
 
-        else:
-            self.send_response(404)
-            self.end_headers()
+servidor = HTTPServer(("localhost", 8000), MiServidor)
 
-
-servidor = HTTPServer(("localhost", 8000), Servidor)
-
-print("Servidor de Veterinaria corriendo en http://localhost:8000")
+print("Servidor corriendo en http://localhost:8000")
 
 servidor.serve_forever()
